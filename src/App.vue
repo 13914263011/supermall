b398941bc1f529b23d15a23bb27011ead70c1f25
wuqi <13914263011@163.com>
2021-01-18 17:37:07 +0800

    加入tabbar组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  components: {
-  }
-}
+  import MainTabBar from '@/components/content/mainTabbar/MainTabBar';
+
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar,
+    },
+  };
 </script>
 
-<style>
 
+<style>
+@import "/assets/css/base.css";
+#app {
+  position: relative;
+}
 </style>
