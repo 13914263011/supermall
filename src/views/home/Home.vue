74ae911bac78a6f84af9a3e6c72ee9ebe75be790
wuqi <13914263011@163.com>
2021-01-26 13:54:14 +0800

    更新轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,52 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <swiper :banners="banners"/>
+  </div>
 </template>
 
 <script>
+  import NavBar from '@/components/common/navbar/NavBar';
+  import Swiper from './childComps/Swiper';
+
+  import {getHomeMultidata} from '../../network/home';
+
   export default {
-    name: "Home"
-  }
+    name: 'Home',
+    components: {
+      NavBar,
+      Swiper,
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      };
+    },
+    created() {
+      // 1.请求多个数据
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+  };
 </script>
 
 <style scoped>
+  #home {
+    /*position: relative;*/
+    height: 100vh;
+  }
+
+  .nav-bar {
+    background-color: #ff8198;
+    font-weight: 700;
+    color: #fff;
+    width: 100%;
+  }
 
 </style>
